178fc8d110fe9ecba7e89c87a0bbc57f37734ffa
pascaloseko <pascal.ondieki21@gmail.com>
2023-12-22 16:08:05 +0300

    add login/signup validations

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -9,6 +9,7 @@ interface AuthFormProps {
     email?: string;
   }) => Promise<void>;
   onSuccess?: () => void;
+  loginError?: string;
 }
 
 interface InputProps {
@@ -19,6 +20,7 @@ interface InputProps {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   required: boolean;
+  error?: string;
 }
 
 const AuthInput: React.FC<InputProps> = ({
@@ -29,13 +31,16 @@ const AuthInput: React.FC<InputProps> = ({
   value,
   onChange,
   required,
+  error,
 }) => (
   <>
     <label className="block mb-2" htmlFor={name}>
       {label}
     </label>
     <input
-      className="w-full px-4 py-2 mb-4 border rounded-md"
+      className={`w-full px-4 py-2 mb-4 border rounded-md ${
+        error ? "border-red-500" : ""
+      }`}
       minLength={minLength}
       name={name}
       id={name}
@@ -45,51 +50,106 @@ const AuthInput: React.FC<InputProps> = ({
       onChange={onChange}
       required={required}
     />
+    {error && <p className="text-red-500">{error}</p>}
   </>
 );
 
-const AuthLink: React.FC<{ href: string; text: string; title: string }> = ({ href, text, title }) => (
-    <p className="mt-4 text-center">
-      {text}{" "}
-      <Link href={href} className="text-blue-500">
-        {title} here
-      </Link>
-    </p>
-  );
-  
+const AuthLink: React.FC<{ href: string; text: string; title: string }> = ({
+  href,
+  text,
+  title,
+}) => (
+  <p className="mt-4 text-center">
+    {text}{" "}
+    <Link href={href} className="text-blue-500">
+      {title} here
+    </Link>
+  </p>
+);
 
-const AuthForm: React.FC<AuthFormProps> = ({ title, onSubmit, onSuccess }) => {
+const AuthForm: React.FC<AuthFormProps> = ({
+  title,
+  onSubmit,
+  onSuccess,
+  loginError,
+}) => {
   const [formData, setFormData] = useState({
     username: "",
     email: "",
     password: "",
   });
+  const [formErrors, setFormErrors] = useState<{
+    username?: string;
+    email?: string;
+    password?: string;
+  }>({});
+
+  const [submitError, setSubmitError] = useState<string | null>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
+    // Clear the associated error when the user starts typing
+    setFormErrors({
+      ...formErrors,
+      [e.target.name]: "",
+    });
+    setSubmitError(null);
+  };
+
+  const validateForm = (): boolean => {
+    const errors: { [key: string]: string } = {};
+    const { email, password, username } = formData;
+
+    if (!email || !email.trim()) {
+      errors.email = "Email is required";
+    }
+
+    if (!password || !password.trim()) {
+      errors.password = "Password is required";
+    }
+
+    if (title === "Signup" && (!username || !username.trim())) {
+      errors.username = "Username is required";
+    }
+
+    setFormErrors(errors);
+
+    // Return true if there are no errors
+    return Object.keys(errors).length === 0;
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    try {
-      await onSubmit(formData);
+    console.log(formErrors, loginError);
 
-      // Call the onSuccess callback if provided
-      if (onSuccess) {
-        onSuccess();
+    if (validateForm()) {
+      try {
+        await onSubmit(formData);
+
+        // Call the onSuccess callback if provided
+        if (onSuccess) {
+          onSuccess();
+        }
+      } catch (error) {
+        console.error(`${title} error:`, error);
+
+        // Display error message to the user
+        setSubmitError(
+          "There was an issue submitting the form. Please try again later."
+        );
+        setFormErrors(null);
       }
-    } catch (error) {
-      console.error(`${title} error:`, error);
     }
   };
 
   return (
     <div className="max-w-md mx-auto m-4 p-6 bg-white border rounded-md shadow-md">
       <h2 className="text-2xl font-semibold mb-4">{title}</h2>
+      {submitError && <p className="text-red-500">{submitError}</p>}
       <form onSubmit={handleSubmit}>
         <AuthInput
           label="Email"
@@ -99,6 +159,7 @@ const AuthForm: React.FC<AuthFormProps> = ({ title, onSubmit, onSuccess }) => {
           value={formData.email}
           onChange={handleChange}
           required
+          error={formErrors.email}
         />
 
         {title === "Signup" && (
@@ -110,6 +171,7 @@ const AuthForm: React.FC<AuthFormProps> = ({ title, onSubmit, onSuccess }) => {
             value={formData.username}
             onChange={handleChange}
             required
+            error={formErrors.username}
           />
         )}
 
@@ -121,6 +183,7 @@ const AuthForm: React.FC<AuthFormProps> = ({ title, onSubmit, onSuccess }) => {
           value={formData.password}
           onChange={handleChange}
           required
+          error={formErrors.password}
         />
 
         <button
@@ -129,10 +192,19 @@ const AuthForm: React.FC<AuthFormProps> = ({ title, onSubmit, onSuccess }) => {
         >
           {title}
         </button>
+        {loginError && <p className="ml-10 mt-2 text-red-500">{loginError}</p>}
       </form>
 
-      {title === "Login" && <AuthLink href="/signup" text="Don't have an account?" title="Sign Up" />}
-      {title === "Signup" && <AuthLink href="/login" text="Have an account?" title="Log In" />}
+      {title === "Login" && (
+        <AuthLink
+          href="/signup"
+          text="Don't have an account?"
+          title="Sign Up"
+        />
+      )}
+      {title === "Signup" && (
+        <AuthLink href="/login" text="Have an account?" title="Log In" />
+      )}
     </div>
   );
 };
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import { useAuth } from "../context/AuthContext";
 import AuthForm from "../components/AuthForm";
@@ -7,23 +8,33 @@ import { useRouter } from "next/router";
 const LoginPage = () => {
   const router = useRouter();
   const { login, loading } = useAuth();
+  const [loginError, setLoginError] = useState<string | null>(null);
 
   const handleLogin = async ({ email, password }) => {
-    return await login(email, password);
+    try {
+      await login(email, password);
+      // Clear any previous login errors on success
+      setLoginError(null);
+    } catch (error) {
+      setLoginError("Invalid email or password. Please try again."); // Set an error message
+      throw error; // Re-throw the error to propagate it to AuthForm
+    }
   };
 
   const handleLoginSuccess = () => {
     router.push("/");
   };
+
   return (
     <Layout pageTitle="Login">
-      {loading ? ( // Conditional rendering of loader
+      {loading && loginError === null ? (
         <Loader />
       ) : (
         <AuthForm
           title="Login"
           onSubmit={handleLogin}
           onSuccess={handleLoginSuccess}
+          loginError={loginError}
         />
       )}
     </Layout>
